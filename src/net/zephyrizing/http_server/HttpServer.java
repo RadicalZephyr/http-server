30431b5633d4d43bd289f48fda41537544ca61f4
Zefira Shannon <earthlingzephyr@gmail.com>
2015-06-10 17:23:21 -0500

    Implement a very basic http server

--- src/net/zephyrizing/http_server/HttpServer.java
+++ src/net/zephyrizing/http_server/HttpServer.java
@@ -8,8 +8,13 @@ import java.net.Socket;
 
 public class HttpServer {
     public static void main(String[] args) throws Exception {
-        int portNumber = Integer.parseInt(args[0]);
-        System.err.println("Starting server on port " + args[0]);
+        int portNumber;
+        if (args.length == 1) {
+         portNumber = Integer.parseInt(args[0]);
+        } else {
+            portNumber = 80;
+        }
+        System.err.println("Starting server on port " + portNumber);
 
         try (ServerSocket listenSocket = new ServerSocket(portNumber)) {
             System.err.println("Listening for clients...");
@@ -20,13 +25,20 @@ public class HttpServer {
                      new InputStreamReader(socket.getInputStream()));
                  BufferedReader stdIn = new BufferedReader(
                      new InputStreamReader(System.in))) {
-                System.err.println("Connected to client. Proceeding to echo...");
+                System.err.println("Connected to client.");
 
-                String clientInput;
-                while ((clientInput = in.readLine()) != null) {
-                    System.out.println("Got: "+clientInput);
-                    out.println("Echo: "+clientInput);
-                }
+                String request = in.readLine();
+                String[] params = request.split(" ");
+
+                assert(params.length == 3);
+
+                String method = params[0];
+                String path = params[1];
+                String protocolVersion = params[2];
+
+                System.out.format("Client requested to %s file %s over %s.\n",
+                                  method, path, protocolVersion);
+                out.format("%s 200 OK\r\n", protocolVersion);
             }
         }
     }
