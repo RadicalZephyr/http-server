f053da7804d77f96f1e311d5d7af47dcc2241b59
Zefira Shannon <earthlingzephyr@gmail.com>
2015-06-11 18:05:15 -0500

    Start moving towards a more factored design

---
+++ src/net/zephyrizing/http_server/HttpServerSocket.java
@@ -0,0 +1,5 @@
+package net.zephyrizing.http_server;
+
+public interface HttpServerSocket {
+
+}
---
+++ src/net/zephyrizing/http_server_test/HttpServerTest.java
@@ -0,0 +1,20 @@
+package net.zephyrizing.http_server_test;
+
+import org.junit.Test;
+import net.zephyrizing.http_server.HttpServer;
+import net.zephyrizing.http_server.HttpServerSocket;
+
+import static org.junit.Assert.*;
+
+public class HttpServerTest {
+
+    public class MockHttpServerSocket implements HttpServerSocket {
+
+    }
+
+    @Test
+    public void testCreateServer() {
+        MockHttpServerSocket serveSocket = new MockHttpServerSocket();
+        HttpServer server = new HttpServer(serveSocket);
+    }
+}
