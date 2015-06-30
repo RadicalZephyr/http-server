ddb8b464bf818687f806a86b55ba6aa447cba746
Zefira Shannon <earthlingzephyr@gmail.com>
2015-06-30 14:16:39 -0500

    Add the ability to create HttpRequests from data
    And have them return it

--- src/net/zephyrizing/http_server/HttpRequest.java
+++ src/net/zephyrizing/http_server/HttpRequest.java
@@ -2,4 +2,27 @@ package net.zephyrizing.http_server;
 
 public class HttpRequest {
 
+    private String method;
+    private String path;
+    private String protocolVersion;
+
+    public HttpRequest(String[] request) {
+        String firstLine = request[0];
+        String[] methodPathProto = firstLine.split(" ");
+        this.method          = methodPathProto[0];
+        this.path            = methodPathProto[1];
+        this.protocolVersion = methodPathProto[2].replace("HTTP/", "");
+    }
+
+    public String method() {
+        return method;
+    }
+
+    public String path() {
+        return path;
+    }
+
+    public String protocolVersion() {
+        return protocolVersion;
+    }
 }
