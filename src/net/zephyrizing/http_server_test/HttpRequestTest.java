16eac216aa1f86e1f39ecddadc88c9ccb5162a18
Geoff Shannon <geoffpshannon@gmail.com>
2015-06-30 14:16:39 -0500

    Add the ability to create HttpRequests from data
    And have them return it

---
+++ src/net/zephyrizing/http_server_test/HttpRequestTest.java
@@ -0,0 +1,28 @@
+package net.zephyrizing.http_server_test;
+
+import net.zephyrizing.http_server.HttpRequest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HttpRequestTest {
+
+    @Test
+    public void canCreateGETRequest() {
+        String[] requestLines = new String[] {"GET / HTTP/1.1"};
+        HttpRequest request = new HttpRequest(requestLines);
+        assertEquals("GET", request.method());
+        assertEquals("/", request.path());
+        assertEquals("1.1", request.protocolVersion());
+    }
+
+    @Test
+    public void canCreatePOSTRequest() {
+        String[] requestLines = new String[] {"POST /root HTTP/1.0"};
+        HttpRequest request = new HttpRequest(requestLines);
+        assertEquals("POST", request.method());
+        assertEquals("/root", request.path());
+        assertEquals("1.0", request.protocolVersion());
+    }
+}
