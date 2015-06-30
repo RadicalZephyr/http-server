aee0d12070aa3b7fa2d496860e7cd3a1b1bfd83e
Zefira Shannon <earthlingzephyr@gmail.com>
2015-06-30 14:33:32 -0500

    Mock socket stuff enough to construct an HttpRequest

--- src/net/zephyrizing/http_server/HttpServerSocketImpl.java
+++ src/net/zephyrizing/http_server/HttpServerSocketImpl.java
@@ -1,8 +1,12 @@
 package net.zephyrizing.http_server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class HttpServerSocketImpl implements HttpServerSocket {
 
@@ -20,6 +24,16 @@ public class HttpServerSocketImpl implements HttpServerSocket {
 
     @Override
     public HttpRequest accept() {
+        try (
+            Socket socket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()))) {
+            String[] requestLines = new String[] { in.readLine() };
+            return new HttpRequest(requestLines);
+        } catch (IOException e) {
+
+        }
         return null;
     }
 }
