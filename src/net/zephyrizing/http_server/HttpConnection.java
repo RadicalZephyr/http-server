3c0d8bc6d593f3cecf9e6e7cb929f65ae022c504
Zefira Shannon <earthlingzephyr@gmail.com>
2015-07-01 13:39:52 -0500

    Add an HttpConnection class
    Test that it closes the underlying reader and writer.

---
+++ src/net/zephyrizing/http_server/HttpConnection.java
@@ -0,0 +1,10 @@
+package net.zephyrizing.http_server;
+
+import java.io.Closeable;
+
+public interface HttpConnection extends Closeable {
+
+    //public HttpRequest getRequest();
+
+    //public void sendResponse(HttpResponse);
+}
---
+++ src/net/zephyrizing/http_server_test/HttpConnectionClosingTest.java
@@ -0,0 +1,100 @@
+package net.zephyrizing.http_server_test;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.Socket;
+
+import net.zephyrizing.http_server.HttpConnection;
+import net.zephyrizing.http_server.HttpConnectionImpl;
+
+import org.hamcrest.Matcher;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+public class HttpConnectionClosingTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    String[] requestStrings = new String[] { "GET / HTTP/1.1",
+                                             "" };
+
+    TestSocket socket;
+    TestableBufferedReader socketIn;
+    TestablePrintWriter socketOut;
+
+    HttpConnection connection;
+
+    class TestSocket extends Socket {
+        public boolean closeCalled = false;
+
+        @Override
+        public void close() {
+            closeCalled = true;
+        }
+    }
+
+    class TestableBufferedReader extends BufferedReader {
+        public TestableBufferedReader(Reader in) {
+            super(in);
+        }
+
+        public boolean closeCalled = false;
+
+        @Override
+        public void close() throws IOException {
+            super.close();
+            closeCalled = true;
+        }
+    }
+
+    class TestablePrintWriter extends PrintWriter {
+        public TestablePrintWriter(OutputStream out) {
+            super(out);
+        }
+
+        public boolean isClosed() {
+            return out == null;
+        }
+    }
+
+    @Before
+    public void initialize() throws IOException {
+        socket    = new TestSocket();
+        socketIn  = new TestableBufferedReader(new StringReader(""));
+        socketOut = new TestablePrintWriter(new ByteArrayOutputStream());
+
+        try (HttpConnection localConn = new HttpConnectionImpl(socket, socketIn, socketOut);) {
+            connection = localConn;
+        }
+    }
+
+    Matcher<Boolean> isTrue = equalTo(true);
+
+    @Test
+    public void closesSocket() throws Exception {
+        assertThat(socket.closeCalled, isTrue);
+    }
+
+    @Test
+    public void closesReader() throws Exception {
+        assertThat(socketIn.closeCalled, isTrue);
+    }
+
+    @Test
+    public void closesWriter() throws Exception {
+        assertThat(socketOut.isClosed(), isTrue);
+    }
+}
