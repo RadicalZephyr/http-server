a6320717a37470ad81045a13a9f99bb41928d0e8
Geoff Shannon <geoffpshannon@gmail.com>
2015-07-02 15:37:16 -0500

    Add a test that a Response uses the same protocol as it's Request

---
+++ src/net/zephyrizing/http_server_test/HttpResponseTest.java
@@ -0,0 +1,23 @@
+package net.zephyrizing.http_server_test;
+
+import java.util.Arrays;
+
+import net.zephyrizing.http_server.HttpRequest;
+import net.zephyrizing.http_server.HttpResponse;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+public class HttpResponseTest {
+
+    @Test
+    public void isProtocolSame() {
+        String[] lines = {"GET / HTTP/1.1"};
+        HttpRequest request = new HttpRequest(Arrays.asList(lines));
+        HttpResponse response = HttpResponse.responseFor(request);
+        assertThat(response.protocolVersion(), equalTo(request.protocolVersion()));
+    }
+}
