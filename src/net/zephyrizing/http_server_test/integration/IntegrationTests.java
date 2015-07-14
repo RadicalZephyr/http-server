77b53d00626774a8f020fd8fff70db237bd34b21
Geoff Shannon <geoffpshannon@gmail.com>
2015-07-14 11:56:38 -0500

    Add an IntegrationTests suite

---
+++ src/net/zephyrizing/http_server_test/integration/IntegrationTests.java
@@ -0,0 +1,10 @@
+package net.zephyrizing.http_server_test.integration;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({SmokeTest.class})
+public class IntegrationTests {
+
+}
