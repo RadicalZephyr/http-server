e1c601dffc087b5092bc50ef4fdb4d5e97416344
Zefira Shannon <earthlingzephyr@gmail.com>
2015-07-15 12:16:58 -0500

    Fill out the response generation in HttpProtocol
    Now it actually returns an ending empty line as it should, and
    everything is treated as streams.  Currently the header and data streams
    are simply empty, but that can be changed and the protocol should Just
    Work(tm)

--- src/net/zephyrizing/http_server/HttpResponse.java
+++ src/net/zephyrizing/http_server/HttpResponse.java
@@ -1,6 +1,7 @@
 package net.zephyrizing.http_server;
 
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class HttpResponse {
     private Path content;
@@ -21,5 +22,15 @@ public class HttpResponse {
 
     public void setContent(Path file) {
         content = file;
+    }
+
+    public Stream<String> getHeaderStream() {
+        Stream.Builder<String> stb = Stream.builder();
+        return stb.build();
+    }
+
+    public Stream<String> getDataStream() {
+        Stream.Builder<String> stb = Stream.builder();
+        return stb.build();
     }
 }
