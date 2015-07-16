224a6a6fbbc2d850715f4d2bf7fade04d5999c34
Zefira Shannon <earthlingzephyr@gmail.com>
2015-07-16 13:35:59 -0500

    Produce semi-reasonable output for directories

--- src/net/zephyrizing/http_server_test/HttpResponseTest.java
+++ src/net/zephyrizing/http_server_test/HttpResponseTest.java
@@ -34,12 +34,29 @@ public class HttpResponseTest {
     @Test
     public void hasFileContent() throws Exception {
         List<String> content = Arrays.asList(new String[] {"abc", "def", "ghi"});
-        Path contentFile = Files.createTempFile("http-response-",
-                                            "-test");
+        Path contentFile = Files.createTempFile("http-response-test", "");
         Files.write(contentFile, content);
 
         response.setContent(contentFile);
         assertThat(response.getDataStream().collect(Collectors.toList()),
                    equalTo(content));
+    }
+
+    @Test
+    public void hasDirectoryContent() throws Exception {
+        Path contentDir = Files.createTempDirectory("http-response-test");
+        Path contentFile1 = Files.createTempFile(contentDir, "http-response-test", "");
+        Path contentFile2 = Files.createTempFile(contentDir, "http-response-test", "");
+
+        String link1 = String.format("<a href=\"/%s\">%s</a>",
+                                     contentFile1.toString(),
+                                     contentFile1.getFileName());
+        String link2 = String.format("<a href=\"/%s\">%s</a>",
+                                     contentFile2.toString(),
+                                     contentFile2.getFileName());
+
+        response.setContent(contentDir);
+        assertThat(response.getDataStream().collect(Collectors.toList()),
+                   everyItem(anyOf(equalTo(link1), equalTo(link2))));
     }
 }
