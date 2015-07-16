ecb9191e3efad7afa68147fcd0e37dd92d8731a0
Geoff Shannon <geoffpshannon@gmail.com>
2015-07-16 15:34:35 -0500

    Fix visibility on ContentProvider methods and fields

--- src/net/zephyrizing/http_server/page/ContentProvider.java
+++ src/net/zephyrizing/http_server/page/ContentProvider.java
@@ -3,5 +3,5 @@ package net.zephyrizing.http_server.page;
 import java.util.stream.Stream;
 
 public interface ContentProvider {
-    Stream<String> getContent();
+    public Stream<String> getContent();
 }
