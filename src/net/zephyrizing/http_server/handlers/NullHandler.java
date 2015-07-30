3c4868a419c24678d52fb339d490f9a76c14233a
Zefira Shannon <earthlingzephyr@gmail.com>
2015-07-30 13:45:16 -0500

    Add override annotation to handlers

--- src/net/zephyrizing/http_server/handlers/NullHandler.java
+++ src/net/zephyrizing/http_server/handlers/NullHandler.java
@@ -4,6 +4,8 @@ import net.zephyrizing.http_server.HttpRequest;
 import net.zephyrizing.http_server.HttpResponse;
 
 public class NullHandler implements Handler {
+
+    @Override
     public HttpResponse handle(HttpRequest request) {
         return HttpResponse.responseFor(request);
     }
