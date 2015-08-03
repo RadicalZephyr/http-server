d56379cdd35fbd50cf625dc19d1eb27e91a57e62
Zefira Shannon <earthlingzephyr@gmail.com>
2015-08-03 10:27:36 -0500

    Add headers to HttpRequest's

--- src/net/zephyrizing/http_server/HttpRequest.java
+++ src/net/zephyrizing/http_server/HttpRequest.java
@@ -4,7 +4,9 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HttpRequest {
     public static enum Method {OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE};
@@ -12,16 +14,22 @@ public class HttpRequest {
     private final Method method;
     private final Path   path;
     private final String protocolVersion;
+    private Map<String, List<String>> headers;
     private final InputStream body;
 
     public HttpRequest(Method method, String path, String protocolVersion) {
-        this(method, path, protocolVersion, new ByteArrayInputStream(new byte[0]));
+        this(method, path, protocolVersion,
+             new HashMap<String, List<String>>(),
+             new ByteArrayInputStream(new byte[0]));
     }
 
-    public HttpRequest(Method method, String path, String protocolVersion, InputStream body) {
+    public HttpRequest(Method method, String path, String protocolVersion,
+                       Map<String, List<String>> headers,
+                       InputStream body) {
         this.method          = method;
         this.path            = Paths.get(path);
         this.protocolVersion = protocolVersion;
+        this.headers         = headers;
         this.body            = body;
     }
 
@@ -40,6 +48,10 @@ public class HttpRequest {
 
     public String protocolVersion() {
         return protocolVersion;
+    }
+
+    public Map<String, List<String>> headers() {
+        return this.headers;
     }
 
     public InputStream body() {
