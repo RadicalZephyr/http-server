a17a8db75792506e52c8c5762cc693dd779caeee
Zefira Shannon <earthlingzephyr@gmail.com>
2015-08-03 14:58:11 -0500

    Delete no longer used method

--- src/net/zephyrizing/http_server/HttpProtocol.java
+++ src/net/zephyrizing/http_server/HttpProtocol.java
@@ -18,25 +18,6 @@ import net.zephyrizing.http_server.HttpResponse;
 import static net.zephyrizing.http_server.HttpRequest.Method.*;
 
 public class HttpProtocol {
-    public static HttpRequest requestFromLines(Stream<String> lines) {
-        List<String> linesList = lines.filter(Predicate.isEqual("").negate())
-            .limit(1).collect(Collectors.toList());
-
-        if (linesList.isEmpty()) {
-            return null;
-        }
-
-        String requestLine = linesList.get(0);
-        RequestBuilder b = new RequestBuilder();
-
-        try {
-            parseRequestLine(b, requestLine);
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
-
-        return b.build();
-    }
 
     public static HttpRequest requestFromInputStream(InputStream stream) {
         BufferedReader r = new BufferedReader(new InputStreamReader(stream));
