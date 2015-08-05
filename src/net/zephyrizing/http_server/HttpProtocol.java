841ca90126fea0099d5a57479ec92ef08b3a65df
Geoff Shannon <geoffpshannon@gmail.com>
2015-08-04 23:29:22 -0500

    Change how the protocol version number is generated
    Previously, it was simply taken directly from the version of the
    request. But according to RFC 7230 a server should always respond with
    the highest minor version of the protocol that it is compliant with (for
    the request's major version).
    
    See https://tools.ietf.org/html/rfc7230#page-15 for details.

--- src/net/zephyrizing/http_server/HttpProtocol.java
+++ src/net/zephyrizing/http_server/HttpProtocol.java
@@ -25,6 +25,9 @@ import static net.zephyrizing.http_server.HttpRequest.Method.*;
 
 public class HttpProtocol {
 
+    public static final int MAJOR_VERSION = 1;
+    public static final int MINOR_VERSION = 1;
+
     public static final Collector<CharSequence, ?, String> collectAsHttpHeader = Collectors.joining("\r\n", "", "\r\n\r\n");
 
     public static final Pattern HEADER_RE = Pattern.compile("^([^\\s:]+):\\s*(.*?)\\s*$");
