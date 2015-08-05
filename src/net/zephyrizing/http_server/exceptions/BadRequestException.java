2d45503a49b406ed98e8d7e52a89c70237b9f617
Zefira Shannon <earthlingzephyr@gmail.com>
2015-08-04 23:46:12 -0500

    Add a BadRequestException

---
+++ src/net/zephyrizing/http_server/exceptions/BadRequestException.java
@@ -0,0 +1,20 @@
+package net.zephyrizing.http_server.exceptions;
+
+public class BadRequestException extends HttpServerException {
+
+    public BadRequestException() {
+        super();
+    }
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(Throwable cause) {
+        super(cause);
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
