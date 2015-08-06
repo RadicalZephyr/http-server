6a6fdb751189d70bb9269abb19f7cd92c3f5f51a
Geoff Shannon <geoffpshannon@gmail.com>
2015-08-06 09:44:53 -0500

    Introduce a new type name for the headers map specialization

---
+++ src/net/zephyrizing/http_server/Headers.java
@@ -0,0 +1,6 @@
+package net.zephyrizing.http_server;
+
+import java.util.List;
+import java.util.Map;
+
+public interface Headers extends Map<String, List<String>> {}
---
+++ src/net/zephyrizing/http_server/HeadersMap.java
@@ -0,0 +1,6 @@
+package net.zephyrizing.http_server;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class HeadersMap extends HashMap<String, List<String>> implements Headers {}
