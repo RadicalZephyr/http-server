dbb24c0bd2013122422ba34d01c1e5d9355d8c38
Geoff Shannon <geoffpshannon@gmail.com>
2015-08-06 14:34:12 -0500

    Start working on basic auth

---
+++ src/net/zephyrizing/http_server_test/middleware/BasicAuthTest.java
@@ -0,0 +1,11 @@
+package net.zephyrizing.http_server_test.middleware;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+public class BasicAuthTest {
+
+}
