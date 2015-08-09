149e24daa12a0f3274209b951617b2b959dd6d4b
Zefira Shannon <earthlingzephyr@gmail.com>
2015-08-09 14:55:55 -0500

    Split command-line starting into a separate package

--- src/net/zephyrizing/http_server/HttpServer.java
+++ src/net/zephyrizing/http_server/HttpServer.java
@@ -11,57 +11,14 @@ import java.util.Arrays;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-import joptsimple.OptionParser;
-import joptsimple.OptionSet;
-import joptsimple.OptionSpec;
-
 import net.zephyrizing.http_server.exceptions.HttpServerException;
 import net.zephyrizing.http_server.exceptions.BadRequestException;
 import net.zephyrizing.http_server.handlers.Handler;
 import net.zephyrizing.http_server.handlers.CobSpecHandler;
 
-import static java.util.Arrays.asList;
-
 public class HttpServer implements Closeable {
 
     private static int DEFAULT_THREADS = 24;
-
-    public static void main(String[] args) throws InterruptedException {
-        String defaultPublicRoot = String.format("%s/src/cob_spec/public/",
-                                                 System.getenv("HOME"));
-
-        OptionParser parser = new OptionParser();
-        OptionSpec<Integer> portOpt = parser.acceptsAll(asList("p", "port"))
-            .withRequiredArg().ofType(Integer.class).defaultsTo(5000);
-        OptionSpec<String>  rootOpt = parser.acceptsAll(asList("d", "directory"))
-            .withRequiredArg().ofType(String.class).defaultsTo(defaultPublicRoot);
-        OptionSpec<Integer> threadsOpt = parser.acceptsAll(asList("t", "threads"))
-            .withRequiredArg().ofType(Integer.class).defaultsTo(DEFAULT_THREADS);
-
-        OptionSet options = parser.parse(args);
-
-        int portNumber = options.valueOf(portOpt);
-        String public_root_path = options.valueOf(rootOpt);
-        int threadPoolSize = options.valueOf(threadsOpt);
-
-        Path public_root = Paths.get(public_root_path);
-
-        if (!Files.isDirectory(public_root)) {
-            System.err.println("The directory given must exist!");
-            return;
-        }
-
-        System.err.format("Starting server on port %d...", portNumber);
-
-        Handler handler = new CobSpecHandler(public_root);
-        try (HttpServer server = HttpServer.createServer(portNumber, handler)) {
-
-            server.listen();
-            server.serve();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public static HttpServer createServer(int port, Handler handler) throws IOException {
         ServerSocket serverSocket = null;
